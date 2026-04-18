1853ccdd3487bc2db8fe8b18c2f900d72443411b
ErdemKaraaslan01 <erdemkaraaslan01@hotmail.com>
2026-04-18 17:05:27 +0300

    feat: add random weight generation

--- seesaw.js
+++ seesaw.js
@@ -3,3 +3,7 @@ const plank = document.getElementById("plank");
 plank.addEventListener("click", () => {
   console.log("plank clicked");
 });
+
+function randomWeight() {
+  return Math.floor(Math.random() * 10) + 1;
+}
