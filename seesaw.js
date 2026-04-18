e817482f87820de0877458b1f41e21b633ec13b7
ErdemKaraaslan01 <erdemkaraaslan01@hotmail.com>
2026-04-18 17:25:19 +0300

    feat: render weights on seesaw

--- seesaw.js
+++ seesaw.js
@@ -1,7 +1,9 @@
 const plank = document.getElementById("plank");
+const seesaw = document.getElementById("seesaw");
+
 let objects = []; 
 
-plank.addEventListener("click", (e) => {
+plank.addEventListener("click",  (e) => {
   const weight = randomWeight();
 
   objects.push({
@@ -9,9 +11,20 @@ plank.addEventListener("click", (e) => {
     weight: weight
   });
 
-  console.log(objects);
+  render();
 });
 
 function randomWeight() {
   return Math.floor(Math.random() * 10) + 1;
+}
+
+function render() {
+  objects.forEach(obj => {
+    const el = document.createElement("div");
+    el.className = "weight";
+    el.style.left = obj.x + "px";
+    el.innerText = obj.weight + "kg";
+
+    seesaw.appendChild(el);
+  });
 }
