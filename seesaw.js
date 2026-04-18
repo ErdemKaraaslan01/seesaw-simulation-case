f52102abc13c7d4f1d968c348e321227241ae917
ErdemKaraaslan01 <erdemkaraaslan01@hotmail.com>
2026-04-18 19:30:45 +0300

    feat: fix weight generation logic and synchronize UI

--- seesaw.js
+++ seesaw.js
@@ -1,36 +1,48 @@
 const plank = document.getElementById("plank");
 const seesaw = document.getElementById("seesaw");
 
+const leftDisplay = document.getElementById("leftWeight");
+const rightDisplay = document.getElementById("rightWeight");
+const tiltDisplay = document.getElementById("tiltAngle");
+const nextDisplay = document.getElementById("nextWeight");
+
 const PLANK_WIDTH = 400;
 const CENTER = PLANK_WIDTH / 2;
 
 let objects = [];
 
+let nextWeight = randomWeight();
+nextDisplay.innerText = `${nextWeight} kg`;
+
+function randomWeight() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
 plank.addEventListener("click", (e) => {
     const rect = plank.getBoundingClientRect();
     const x = e.clientX - rect.left;
 
-    const weight = randomWeight();
+    const weight = nextWeight;
 
     objects.push({
         x: x,
         weight: weight
     });
 
+    nextWeight = randomWeight();
+    nextDisplay.innerText = `${nextWeight} kg`;
+
     render();
 });
 
-function randomWeight() {
-    return Math.floor(Math.random() * 10) + 1;
-}
 
 function render() {
     document.querySelectorAll(".weight").forEach(el => el.remove());
 
-    let leftTorque = 0; 
-    let rightTorque = 0; 
-    let leftWeight = 0;  
-    let rightWeight = 0; 
+    let leftTorque = 0;
+    let rightTorque = 0;
+    let leftWeight = 0;
+    let rightWeight = 0;
 
     objects.forEach(obj => {
         const el = document.createElement("div");
@@ -44,21 +56,21 @@ function render() {
 
         if (obj.x < CENTER) {
             leftWeight += obj.weight;
-            leftTorque += obj.weight * distance; 
+            leftTorque += obj.weight * distance;
         } else {
             rightWeight += obj.weight;
-            rightTorque += obj.weight * distance; 
+            rightTorque += obj.weight * distance;
         }
     });
 
     const angle = Math.max(
         -30,
-        Math.min(30, (rightTorque - leftTorque)/100)
+        Math.min(30, (rightTorque - leftTorque) / 100)
     );
 
     seesaw.style.transform = `rotate(${angle}deg)`;
 
-    leftDisplay.innerText = `${leftWeight} kg`; 
-    rightDisplay.innerText = `${rightWeight} kg`;  
+    leftDisplay.innerText = `${leftWeight} kg`;
+    rightDisplay.innerText = `${rightWeight} kg`;
     tiltDisplay.innerText = `${angle.toFixed(1)}°`;
 }
